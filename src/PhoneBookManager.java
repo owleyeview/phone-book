16b6dd21e0c46dae438c67b9c5e01947b128440a
owleyeview <rashaanlightpool@gmail.com>
2022-10-14 20:58:21 -0700

    added some comments

--- src/PhoneBookManager.java
+++ src/PhoneBookManager.java
@@ -3,26 +3,27 @@ import java.util.*;
 public class PhoneBookManager {
     PhoneBookNode head;
 
+    // given a PhoneBookNode, adds it to the end of the list
     public void append(PhoneBookNode node) {
-        if (head == null) {
-            head = node;
+        if (head == null) { // no nodes in the list yet
+            head = node;  // this node becomes the head of the list
             return;
         }
         PhoneBookNode current = head;
         while (current.next != null) {
             current = current.next;
         }
-        current.next =  node;
+        current.next =  node;  // adding this node to the end of the list
     }
 
     // display all the phone book entries
     public void printList() {
-        if (head == null) {
+        if (head == null) { // no nodes in the list
             System.out.println("!! No entries to display. Please add a new entry.");
             return;
         }
         PhoneBookNode current = head;
-        int index = 1;
+        int index = 1;  // a reference number for each entry
         System.out.println();
         System.out.printf("%3d. ", index);
         System.out.println(current.toString());
